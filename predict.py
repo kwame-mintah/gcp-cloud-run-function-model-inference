827446906a3c436b04531916f7caee48244275a7
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2024-11-08 21:39:45 +0000

    feat(download_blob_and_return_object): new function to download model artifact from bucket
    `download_blob_and_return_object` will download the blob object and re-use `load_object` to return the serialized object ready for prediction. new environment variables, `GCP_MLFLOW_MODEL_ARTIFACT_BUCKET_NAME` and `USE_LOCAL_FILE_PATH_MODEL` added to application.

--- predict.py
+++ predict.py
@@ -4,7 +4,7 @@ import pandas as pd
 from sklearn.linear_model import ElasticNet
 
 from columns import WineQualityData
-from utils import load_object
+from utils import load_object, EnvironmentVariables, download_blob_and_return_object
 
 
 def predict_wine_quality(wine_list: List[WineQualityData]) -> List[float]:
@@ -17,5 +17,10 @@ def predict_wine_quality(wine_list: List[WineQualityData]) -> List[float]:
     dataframe = pd.DataFrame(
         data=[wine.model_dump(by_alias=True) for wine in wine_list]
     )
-    model: ElasticNet = load_object(file_path="model.pkl")
+
+    model: ElasticNet = (
+        load_object(file_path="model.pkl")
+        if EnvironmentVariables().USE_LOCAL_FILE_PATH_MODEL
+        else download_blob_and_return_object()
+    )
     return model.predict(dataframe)
--- utils.py
+++ utils.py
@@ -1,11 +1,25 @@
 import logging
+from typing import Any
 
 import dill
+from google.cloud import storage
+from pydantic import Field
+from pydantic_settings import BaseSettings
 
 
-def load_object(file_path: str):
+class EnvironmentVariables(BaseSettings):
+    GCP_MLFLOW_MODEL_ARTIFACT_BUCKET_NAME: str = Field(
+        description="The GCP bucket name, where the model artifact has been uploaded"
+    )
+    USE_LOCAL_FILE_PATH_MODEL: bool = Field(
+        default=False,
+        description="Use the model artifact found locally, rather than fetching from GCP bucket.",
+    )
+
+
+def load_object(file_path: str) -> Any:
     """
-    Load file into python and return object.
+    Load file into python and return serialized object.
 
     :param file_path: file location
     :return: object
@@ -17,3 +31,24 @@ def load_object(file_path: str):
     except Exception as e:
         logging.info(f"Error in load_object: {str(e)}")
         raise e
+
+
+def download_blob_and_return_object(
+    bucket_name: str = EnvironmentVariables().GCP_MLFLOW_MODEL_ARTIFACT_BUCKET_NAME,
+    source_blob_name: str = "model.pkl",
+    destination_file_name: str = "gcp_model.pkl",
+) -> Any:
+    """
+    Downloads a blob from the bucket and return serialized object
+
+    :param destination_file_name:
+    :param bucket_name: The ID of your GCS bucket
+    :param source_blob_name: The ID of your GCS object
+    :return: serialized object
+    """
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(source_blob_name)
+    blob.download_to_filename(destination_file_name)
+    return load_object(destination_file_name)
