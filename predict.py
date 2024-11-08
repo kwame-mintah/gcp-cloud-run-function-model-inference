05a71db636e2ec23b73f90afdf5343d221cef182
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2024-11-08 15:11:04 +0000

    refactor(predict_wine_quality): rename variable `res` -> `wine`
    variable named to be clearer, instead of being named `res`

--- predict.py
+++ predict.py
@@ -10,9 +10,12 @@ from utils import load_object
 def predict_wine_quality(wine_list: List[WineQualityData]) -> List[float]:
     """
     Load model artifact from directory and predict with LinearModel
+
     :param wine_list: wine list
     :return: wine quality
     """
-    dataframe = pd.DataFrame(data=[res.model_dump(by_alias=True) for res in wine_list])
+    dataframe = pd.DataFrame(
+        data=[wine.model_dump(by_alias=True) for wine in wine_list]
+    )
     model: ElasticNet = load_object(file_path="model.pkl")
     return model.predict(dataframe)
