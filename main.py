bdae4c51b3025629bee31b3225ad6b5835175c8c
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2024-11-11 22:07:41 +0000

    refactor(app): add endpoint metadata
    add title, description, contact and license info to metadata for endpoint

--- main.py
+++ main.py
@@ -6,7 +6,19 @@ from fastapi import FastAPI, status
 from columns import WineQualityData
 from predict import predict_wine_quality
 
-app = FastAPI()
+app = FastAPI(
+    title="Wine Quality Prediction",
+    description="An example project for running a prediction using a machine learning model stored within a GCP bucket",
+    contact={
+        "name": "Kwame Mintah",
+        "url": "https://github.com/kwame-mintah/kwame-mintah/discussions/categories/general",
+        "email": "email@email.com",
+    },
+    license_info={
+        "name": "GNU GENERAL PUBLIC LICENSE",
+        "url": "https://www.gnu.org/licenses/gpl-3.0.en.html#license-text",
+    },
+)
 
 
 @app.post(
@@ -28,4 +40,5 @@ async def predict(data: List[WineQualityData]) -> List[float]:
 
 
 if __name__ == "__main__":
+    # Useful when running application locally.
     uvicorn.run(app, host="0.0.0.0", port=8000)
